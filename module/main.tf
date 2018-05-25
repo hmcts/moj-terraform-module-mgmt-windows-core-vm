7496c270c3521146417d93e64dc814c243341cc7
Janis Berzins <janis.berzins@hmcts.net>
2018-05-25 14:32:41 +0100

    Updated script extension

--- module/main.tf
+++ module/main.tf
@@ -85,7 +85,7 @@ resource "azurerm_virtual_machine_extension" "reform-nonprod" {
   virtual_machine_name = "${azurerm_virtual_machine.reform-nonprod.name}"
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
-  type_handler_version = "2.0"
+  type_handler_version = "1.9"
 
   settings = <<SETTINGS
      {
